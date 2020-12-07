3a44b73d8bb2e8ff9a864d75e8334891ef238bcf
ilya <ilyaandreev2010@gmail.com>
2020-12-07 21:04:45 +0300

    day7 cleanup; todo: comments

--- day7/main.rs
+++ day7/main.rs
@@ -1,83 +1,77 @@
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::collections::HashMap;
-use std::collections::VecDeque;
-use std::collections::HashSet;
-use std::{fs::File, io, io::prelude::*};
+use std::{collections::HashMap, fs::File, io, io::prelude::*};
 
 lazy_static! {
     static ref RE_CONTAINER: Regex = Regex::new(r"(\w+ \w+)?").unwrap();
     static ref RE_CONTENT: Regex = Regex::new(r"([0-9]) (\w+ \w+)").unwrap();
 }
 
-// !!!!!!topological sort???????
+struct Input {
+    contents: HashMap<String, Vec<(String, usize)>>,
+}
 
-// TODO: fix string types
-// map each bag to the vector of (bag, num_bags) tuples that are contained in it
-// map each bag to the vector of bags where it is directly contained
-fn preprocess(
-    s: &str,
-) -> (
-    HashMap<String, Vec<(String, usize)>>,
-    HashMap<String, Vec<String>>,
-) {
-    let (mut contents_map, mut containers_map) = (HashMap::new(), HashMap::new());
-    let map = s.split("\n").map(|s| s.trim());
-    for line in map {
-        let container = RE_CONTAINER.find(line).unwrap().as_str();
-        for cap in RE_CONTENT.captures_iter(line) {
-            let (content, count) = (cap[2].to_string(), cap[1].parse().unwrap());
-            let contents_v = contents_map.entry(container.to_string()).or_insert(Vec::new());
-            contents_v.push((content.clone(), count));
-            let containers_v = containers_map.entry(content).or_insert(Vec::new());
-            // TODO: duplicates possible anywhere?
-            containers_v.push(container.to_string());
+impl Input {
+    fn new(input: &str) -> Input {
+        let mut contents = HashMap::new();
+        let lines = input.split("\n").map(|s| s.trim());
+
+        for line in lines {
+            let key = RE_CONTAINER.find(line).unwrap().as_str().to_string();
+            contents.insert(key.clone(), Vec::new());
+
+            for cap in RE_CONTENT.captures_iter(line) {
+                let (content, count) = (cap[2].to_string(), cap[1].parse().unwrap());
+                contents
+                    .get_mut(&key)
+                    .unwrap()
+                    .push((content, count));
+            }
         }
+
+        Input { contents }
     }
 
-    (contents_map, containers_map)
+    fn count_containers(&self, inner: &str) -> usize {
+        self.contents.iter().fold(0, |acc, (color, _)| {
+            if self.contains(color, inner) {
+                acc + 1
+            } else {
+                acc
+            }
+        })
+    }
+
+    fn contains(&self, outer: &str, inner: &str) -> bool {
+        self.contents[outer]
+            .iter()
+            .any(|(s, _)| s == inner || self.contains(s, inner))
+    }
+
+    fn count_contents(&self, outer: &str) -> usize {
+        self.count_contents_inner(outer) - 1
+    }
+
+    fn count_contents_inner(&self, outer: &str) -> usize {
+        self.contents[outer].iter().fold(1, |acc, (inner, count)| {
+            acc + count * self.count_contents_inner(inner)
+        })
+    }
 }
 
 fn main() -> io::Result<()> {
-    let mut f = File::open("day7/input.txt")?;
     let mut buffer = String::new();
-    f.read_to_string(&mut buffer)?;
-    let (contents_map, containers_map) = preprocess(&buffer);
-
-    let mut queue: VecDeque<String> = VecDeque::new();
-    let query = "shiny gold".to_string();
-    queue.push_back(query);
-
-    let mut storage_options = HashSet::new();
-    while queue.len() != 0 {
-        let el = queue.pop_front().unwrap();
-        match containers_map.get(&el) {
-            Some(containers) => {
-                for container in containers.into_iter() {
-                    queue.push_back(container.to_string());
-                    storage_options.insert(container);
-                }
-            },
-            None => continue,
-        }
-    }
-
-    println!("Storage options in part 1: {}", storage_options.len());
-    println!("Contents max in part2: {}", count_contents("shiny gold", &contents_map)-1);
+    File::open("day7/input.txt")?.read_to_string(&mut buffer)?;
+    
+    let input = Input::new(&buffer);
+    let target = String::from("shiny gold");
+    println!(
+        "Bags containing {}: {}, bags in {}: {}",
+        target,
+        input.count_containers(&target),
+        target,
+        input.count_contents(&target)
+    );
 
     Ok(())
-}
-
-fn count_contents(s: &str, map: &HashMap<String, Vec<(String, usize)>>,) -> usize {
-    let mut total = 1;
-    match map.get(s) {
-        Some(contents) => {
-            for (content, num) in contents.into_iter() {
-                total += num * count_contents(content, map);
-            }
-        }
-        _ => {}
-    }
-
-    total
 }
