18a95b3a4cf3be9e1ffa68d49a74b393507795ea
ilya <ilyaandreev2010@gmail.com>
2020-12-08 17:39:20 +0300

    cleverfest

--- day8/main.rs
+++ day8/main.rs
@@ -1,9 +1,8 @@
-use std::{fs::File, io, io::prelude::*, time::Instant};
+#![feature(destructuring_assignment)]
+use std::{collections::HashSet, fs::File, io, io::prelude::*, time::Instant};
 
 struct Program {
-    tampered: bool,
-    visited: Vec<bool>,
-    instructions: Vec<Box<dyn Fn(i32, usize) -> (i32, usize)>>,
+    instructions: Vec<(Box<dyn Fn(i32, usize) -> (i32, usize)>, Op)>,
 }
 
 enum Runtime {
@@ -11,44 +10,36 @@ enum Runtime {
     Terminated(i32),
 }
 
+enum Op {
+    JMP(i32),
+    NOP(i32),
+    ACC(i32),
+}
+
 impl Program {
-    // Pass -1 to make no changes to the instruction code.
-    fn new(input: &str, to_change: i32) -> Program {
+    // Create a new program from the instruction code.
+    //
+    // NOP advances index by 1.
+    //
+    // ACC changes the accumulator by the specified value.
+    //
+    // JMP changes the index by the specified value.
+    fn new(input: &str) -> Program {
         let lines = input.split("\n").map(|s| s.trim());
         let mut instructions = Vec::new();
 
-        let mut tampered = false;
-        for (i, line) in lines.enumerate() {
+        for line in lines {
             let delta = line.split(" ").collect::<Vec<&str>>()[1];
             let delta = delta.parse::<i32>().unwrap();
             match &line[..3] {
-                "nop" => {
-                    if i as i32 == to_change {
-                        tampered = true;
-                        instructions.push(Program::make_jmp(delta));
-                    } else {
-                        instructions.push(Program::make_nop());
-                    }
-                }
-                "acc" => instructions.push(Program::make_acc(delta)),
-                "jmp" => {
-                    if i as i32 == to_change {
-                        tampered = true;
-                        instructions.push(Program::make_nop());
-                    } else {
-                        instructions.push(Program::make_jmp(delta));
-                    }
-                }
+                "nop" => instructions.push((Program::make_nop(), Op::NOP(delta))),
+                "acc" => instructions.push((Program::make_acc(delta), Op::ACC(delta))),
+                "jmp" => instructions.push((Program::make_jmp(delta), Op::JMP(delta))),
                 _ => {}
             }
         }
 
-        let len = instructions.len();
-        Program {
-            instructions,
-            tampered,
-            visited: vec![false; len],
-        }
+        Program { instructions }
     }
 
     fn make_acc(delta: i32) -> Box<dyn Fn(i32, usize) -> (i32, usize)> {
@@ -63,24 +54,83 @@ impl Program {
         Box::new(move |acc, index| (acc, index + 1))
     }
 
-    // run the program and either return the accumulator value before the loop
-    // is re-entered or the value of the accumulator after the program
-    // terminates.
-    fn run(&mut self) -> Runtime {
-        let (mut acc, mut index) = (0, 0);
+    // Syntactic sugar to execute a single operation of the program at the given
+    // index.
+    fn step(&self, acc: i32, index: usize) -> (i32, usize) {
+        self.instructions[index].0(acc, index)
+    }
+
+    // Run the program. The outcome is either a detected loop, in which case the
+    // accumulator value before entering the loop for the first time is
+    // returned, or termination, in which case the resulting accumulator is
+    // returned.
+    fn run(&mut self, start: usize) -> Runtime {
+        let mut visited = vec![false; self.instructions.len()];
+        let (mut acc, mut index) = (0, start);
         loop {
-            if index == self.instructions.len() {
+            let len = self.instructions.len();
+            if index == len {
                 return Runtime::Terminated(acc);
             }
 
-            if self.visited[index] {
+            if visited[index] {
                 return Runtime::Looped(acc);
             }
 
-            self.visited[index] = true;
-            let (new_acc, new_index) = self.instructions[index](acc, index);
-            acc = new_acc;
-            index = new_index;
+            visited[index] = true;
+            (acc, index) = self.step(acc, index);
+        }
+    }
+
+    // It can be proved that an instruction T succeeding a flipped instruction F
+    // leads to termination if and only if the program started at the
+    // instruction T terminates.
+    //
+    // We're allowed to transform a single NOP to an ACC or vice versa.
+    // Therefore, all we need is record terminating instructions Ts and find a
+    // flipped instruction F such that its successor is in Ts. Once found,
+    // update the program to run F in place of the original.
+    fn fix_self(&mut self) -> Option<usize> {
+        // Record terminating instructions Ts.
+        let terminating_starts = &mut HashSet::new();
+        for i in 0..self.instructions.len() {
+            match self.run(i) {
+                Runtime::Looped(_) => continue,
+                Runtime::Terminated(_) => terminating_starts.insert(i),
+            };
+        }
+
+        let mut index = 0;
+        loop {
+            let (_, op) = &self.instructions[index];
+            // Attempt to run a flipped instruction F instead of what's given.
+            // If the resulting next index is in Ts, change the program and
+            // return.
+            match op {
+                Op::ACC(_) => {}
+                Op::JMP(delta) => {
+                    let f_nop = Program::make_nop();
+                    let (_, next_index_candidate) = f_nop(0, index);
+                    if terminating_starts.contains(&next_index_candidate) {
+                        self.instructions[index] = (f_nop, Op::NOP(*delta));
+                        return Some(index);
+                    }
+                }
+                Op::NOP(delta) => {
+                    let f_acc = Program::make_acc(*delta);
+                    let (_, next_index_candidate) = f_acc(0, index);
+                    if terminating_starts.contains(&next_index_candidate) {
+                        self.instructions[index] = (f_acc, Op::ACC(*delta));
+                        return Some(index);
+                    }
+                }
+            }
+
+            // Otherwise run the original given instruction. Note: cargo fmt
+            // doesn't recognize the assignement destructuring crate attribute
+            // at the time of writing, so I'm going with this weird syntax.
+            let _acc;
+            (_acc, index) = self.step(0, index);
         }
     }
 }
@@ -88,39 +138,23 @@ impl Program {
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
     File::open("day8/input.txt")?.read_to_string(&mut buffer)?;
+    let mut program = Program::new(&buffer);
 
-    let mut i = -1;
-    let mut program = Program::new(&buffer, i);
     let now_one = Instant::now();
-    if let Runtime::Looped(acc) = program.run() {
+    if let Runtime::Looped(acc) = program.run(0) {
         println!("Accumulator before entering the loop is {}", acc);
         println!("Part 1 solved in {}μs", now_one.elapsed().as_micros());
     };
 
-    // For an improvement idea, see
-    // https://www.reddit.com/r/adventofcode/comments/k8zdx3/day_8_part_2_without_bruteforce/gf19rwx/.
     let now_two = Instant::now();
-    loop {
-        let mut program = Program::new(&buffer, i);
+    let fixed_instruction = program.fix_self().unwrap();
+    if let Runtime::Terminated(acc) = program.run(0) {
+        println!(
+            "Accumulator after termination is {}, solved by fixing instruction {}",
+            acc, fixed_instruction
+        );
+        println!("Part 2 solved in {}μs", now_two.elapsed().as_micros());
+    };
 
-        // Not interested in running the original program.
-        if !program.tampered {
-            i += 1;
-            continue;
-        }
-
-        match program.run() {
-            Runtime::Looped(_) => {
-                i += 1;
-                continue;
-            }
-            Runtime::Terminated(acc) => {
-                println!("Got accumulator {} by fixing instruction {}", acc, i);
-                break;
-            }
-        }
-    }
-    
-    println!("Part 2 solved in: {}ms", now_two.elapsed().as_millis());
     Ok(())
 }
