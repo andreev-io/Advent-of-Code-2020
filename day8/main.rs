28fb794ddf14ecd8aaf12d264e83e11926d7ed1f
ilya <ilyaandreev2010@gmail.com>
2020-12-08 18:22:38 +0300

    final cleanup. letting it go

--- day8/main.rs
+++ day8/main.rs
@@ -2,7 +2,7 @@
 use std::{collections::HashSet, fs::File, io, io::prelude::*, time::Instant};
 
 struct Program {
-    instructions: Vec<(Box<dyn Fn(i32, usize) -> (i32, usize)>, Op)>,
+    instructions: Vec<Op>,
 }
 
 enum Runtime {
@@ -18,12 +18,6 @@ enum Op {
 
 impl Program {
     // Create a new program from the instruction code.
-    //
-    // NOP advances index by 1.
-    //
-    // ACC changes the accumulator by the specified value.
-    //
-    // JMP changes the index by the specified value.
     fn new(input: &str) -> Program {
         let lines = input.split("\n").map(|s| s.trim());
         let mut instructions = Vec::new();
@@ -32,9 +26,9 @@ impl Program {
             let delta = line.split(" ").collect::<Vec<&str>>()[1];
             let delta = delta.parse::<i32>().unwrap();
             match &line[..3] {
-                "nop" => instructions.push((Program::make_nop(), Op::NOP(delta))),
-                "acc" => instructions.push((Program::make_acc(delta), Op::ACC(delta))),
-                "jmp" => instructions.push((Program::make_jmp(delta), Op::JMP(delta))),
+                "nop" => instructions.push(Op::NOP(delta)),
+                "acc" => instructions.push(Op::ACC(delta)),
+                "jmp" => instructions.push(Op::JMP(delta)),
                 _ => {}
             }
         }
@@ -42,22 +36,19 @@ impl Program {
         Program { instructions }
     }
 
-    fn make_acc(delta: i32) -> Box<dyn Fn(i32, usize) -> (i32, usize)> {
-        Box::new(move |acc, index| (acc + delta, index + 1))
-    }
-
-    fn make_jmp(delta: i32) -> Box<dyn Fn(i32, usize) -> (i32, usize)> {
-        Box::new(move |acc, index| (acc, ((index as i32) + delta) as usize))
-    }
-
-    fn make_nop() -> Box<dyn Fn(i32, usize) -> (i32, usize)> {
-        Box::new(move |acc, index| (acc, index + 1))
-    }
-
-    // Syntactic sugar to execute a single operation of the program at the given
-    // index.
-    fn step(&self, acc: i32, index: usize) -> (i32, usize) {
-        self.instructions[index].0(acc, index)
+    // Execute a single instruction.
+    fn step(&self, acc: i32, index: usize, op: &Op) -> (i32, usize) {
+        match op {
+            Op::ACC(delta) => {
+                return (acc + delta, index + 1);
+            }
+            Op::JMP(delta) => {
+                return (acc, ((index as i32) + delta) as usize);
+            }
+            Op::NOP(_) => {
+                return (acc, index + 1);
+            }
+        }
     }
 
     // Run the program. The outcome is either a detected loop, in which case the
@@ -78,7 +69,7 @@ impl Program {
             }
 
             visited[index] = true;
-            (acc, index) = self.step(acc, index);
+            (acc, index) = self.step(acc, index, &self.instructions[index]);
         }
     }
 
@@ -102,35 +93,32 @@ impl Program {
 
         let mut index = 0;
         loop {
-            let (_, op) = &self.instructions[index];
             // Attempt to run a flipped instruction F instead of what's given.
             // If the resulting next index is in Ts, change the program and
             // return.
-            match op {
+            match self.instructions[index] {
                 Op::ACC(_) => {}
                 Op::JMP(delta) => {
-                    let f_nop = Program::make_nop();
-                    let (_, next_index_candidate) = f_nop(0, index);
+                    let (_, next_index_candidate) = self.step(0, index, &Op::NOP(delta));
                     if terminating_starts.contains(&next_index_candidate) {
-                        self.instructions[index] = (f_nop, Op::NOP(*delta));
+                        self.instructions[index] = Op::NOP(delta);
                         return Some(index);
                     }
                 }
                 Op::NOP(delta) => {
-                    let f_acc = Program::make_acc(*delta);
-                    let (_, next_index_candidate) = f_acc(0, index);
+                    let (_, next_index_candidate) = self.step(0, index, &Op::JMP(delta));
                     if terminating_starts.contains(&next_index_candidate) {
-                        self.instructions[index] = (f_acc, Op::ACC(*delta));
+                        self.instructions[index] = Op::JMP(delta);
                         return Some(index);
                     }
                 }
             }
 
             // Otherwise run the original given instruction. Note: cargo fmt
-            // doesn't recognize the assignement destructuring crate attribute
-            // at the time of writing, so I'm going with this weird syntax.
+            // doesn't recognize the assignment destructuring crate attribute at
+            // the time of writing, hence the awkward syntax.
             let _acc;
-            (_acc, index) = self.step(0, index);
+            (_acc, index) = self.step(0, index, &self.instructions[index]);
         }
     }
 }
