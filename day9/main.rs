27ebb5c8bc82338385eb732741874ee062e68694
ilya <ilyaandreev2010@gmail.com>
2020-12-09 10:22:53 +0300

    De Morgan's Law

--- day9/main.rs
+++ day9/main.rs
@@ -3,12 +3,13 @@ use std::{
     fs::File,
     io,
     io::prelude::*,
-    iter::FromIterator,
+    time::Instant,
 };
 
 const SIZE: i64 = 25;
 
 fn main() -> io::Result<()> {
+    let now_one = Instant::now();
     let mut buffer = String::new();
     File::open("day9/input.txt")?.read_to_string(&mut buffer)?;
     let mut q: VecDeque<i64> = VecDeque::new();
@@ -27,7 +28,7 @@ fn main() -> io::Result<()> {
 
     let mut pt_one_answer = 0;
     for num in nums {
-        if !q.iter().any(|n| 2 * n != num && s.contains(&(num - n))) {
+        if q.iter().all(|n| 2 * n == num || !s.contains(&(num - n))) {
             pt_one_answer = num;
             break;
         }
@@ -39,30 +40,36 @@ fn main() -> io::Result<()> {
 
     println!("Part 1 answer: {}", pt_one_answer);
 
-    let mut pt_two_answer = 0;
+    let pt_two_answer;
     let nums: Vec<i64> = buffer
         .split("\n")
         .map(|s| s.trim())
         .map(|s| s.parse::<i64>().unwrap())
         .collect();
 
-    for i in 0..nums.len() {
-        let (mut j, mut sum) = (i + 1, nums[i]);
-        while sum < pt_one_answer {
-            sum += nums[j];
+    let (mut i, mut j) = (0, 1);
+    let mut sum = nums[i] + nums[j];
+    loop {
+        if sum > pt_one_answer {
+            sum -= nums[i];
+            i += 1;
+        }
 
-            if sum == pt_one_answer {
-                let contiguous = Vec::from_iter(nums[i..j + 1].iter().cloned());
-                let min = contiguous.iter().min().unwrap();
-                let max = contiguous.iter().max().unwrap();
-                pt_two_answer = min + max;
-                break;
-            }
-
+        if sum < pt_one_answer {
             j += 1;
+            sum += nums[j];
+        }
+
+        if sum == pt_one_answer {
+            let contiguous = &nums[i..j + 1];
+            let min = contiguous.iter().min().unwrap();
+            let max = contiguous.iter().max().unwrap();
+            pt_two_answer = min + max;
+            break;
         }
     }
 
     println!("Part 2 answer: {}", pt_two_answer);
+    println!("Runtime: {}μs", now_one.elapsed().as_micros());
     Ok(())
 }
