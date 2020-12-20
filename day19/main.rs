f7502508c9771753619616ee3c4109f69544c85c
ilya <ilyaandreev2010@gmail.com>
2020-12-20 10:34:54 +0300

    day 19 with manual changes to input

--- day19/main.rs
+++ day19/main.rs
@@ -1,108 +1,105 @@
 #![feature(destructuring_assignment)]
-use itertools::Itertools;
-use std::collections::BTreeMap;
 use std::{fs::File, io, io::prelude::*};
 
+// We are going to use the Cocke-Younger-Kasami algorithm to parse the
+// context-free grammar. To use it, we'll first need to make sure the input is
+// in Chomsky Normal Form (by hand). Although there exists a formal way to do
+// it...
+
+#[derive(Debug, Clone)]
+enum Rule {
+    Terminating(char),
+    // Vector of vectors of rules that are matched by this rule
+    NonTerminating(Vec<Vec<usize>>),
+}
+
+fn parse(rules: &Vec<Rule>, input: &str) -> bool {
+    let grammar_size = rules.len();
+    let chars: Vec<char> = input.chars().collect();
+    let word_len = chars.len();
+
+    let mut parse_table = vec![vec![vec![false; grammar_size + 1]; word_len + 1]; word_len + 1];
+    for i in 0..word_len {
+        for (idx, rule) in rules.iter().enumerate() {
+            if let Rule::Terminating(ch) = rule {
+                if *ch == chars[i] {
+                    parse_table[1][i][idx] = true;
+                }
+            }
+        }
+    }
+
+    for l in 2..=word_len {
+        for s in 0..=word_len - l + 1 {
+            for p in 1..=l - 1 {
+                for (idx, rule) in rules.iter().enumerate() {
+                    match rule {
+                        Rule::NonTerminating(subrules) => {
+                            for subrule in subrules.iter() {
+                                if subrule.len() != 2 {
+                                    continue;
+                                }
+
+                                let one = subrule[0];
+                                let two = subrule[1];
+
+                                if parse_table[p][s][one] && parse_table[l - p][s + p][two] {
+                                    parse_table[l][s][idx] = true;
+                                }
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+    }
+
+    parse_table[word_len][0][0]
+}
+
 fn main() -> io::Result<()> {
-    let mut char_bag: BTreeMap<String, Vec<usize>> = BTreeMap::new();
-    // let mut subrules: BTreeMap<(usizche, usize), String> = BTreeMap::new();
     let mut buffer = String::new();
-    File::open("day19/input.txt")?.read_to_string(&mut buffer)?;
-    let lines = buffer.split("\n").map(|s| s.trim()).filter(|l| l != &"");
-    let mut grammar_size = 0;
-    for line in lines.clone() {
-        if line.contains(":") {
-            grammar_size += 1;
-        }
-    }
+    File::open("day19/input2.txt")?.read_to_string(&mut buffer)?;
+    let mut lines = buffer.split("\n\n");
 
-    let mut rules = vec!["".to_string(); grammar_size];
-    for line in lines.clone() {
-        if line.contains(":") {
-            let chunks: Vec<&str> = line.split(":").collect();
-            rules[chunks[0].parse::<usize>().unwrap()] = chunks[1].trim().to_string();
-        }
-    }
-
-    let mut subrules: Vec<Vec<String>> = vec![vec!["".to_string(); rules.len()]; rules.len()];
-    let mut bag: Vec<Vec<Vec<usize>>> = vec![vec![Vec::new(); rules.len()]; rules.len()];
-    for line in lines {
-        if !line.contains(":") && line != "" {
-            let mut m = vec![vec![Vec::<usize>::new(); line.len()]; line.len()];
-            for (i, c) in line.chars().enumerate() {
-                let key = c.to_string();
-                if let Some(ixs) = char_bag.get(&key) {
-                    for ix in ixs {
-                        m[i][i].push(*ix);
-                    }
-                } else {
-                    for idx in 0..rules.len() {
-                        if rules[idx].contains(c) {
-                            if let Some(cache) = char_bag.get_mut(&c.to_string()) {
-                                cache.push(idx);
-                            } else {
-                                char_bag.insert(c.to_string(), vec![idx]);
-                            }
-                            m[i][i].push(idx);
-                        }
-                    }
-                }
-            }
-
-            for l in 1..line.len() {
-                for r in 0..line.len() - l {
-                    for t in 0..l {
-                        let alpha = m[r][r + t].clone();
-                        let beta = m[r + t + 1][r + l].clone();
-                        let mut pairs =
-                            beta.iter()
-                                .cartesian_product(alpha.iter())
-                                .collect::<Vec<(&usize, &usize)>>();
-                        let mut pairs_rev = alpha
-                            .iter()
-                            .cartesian_product(beta.iter())
-                            .collect::<Vec<(&usize, &usize)>>();
-                        pairs.append(&mut pairs_rev);
-                        pairs.dedup();
-
-                        for pair in pairs {
-                            if subrules[*pair.0][*pair.1] == "" {
-                                subrules[*pair.0][*pair.1] = make_subrule(pair);
-                            }
-                            let v = &bag[*pair.0][*pair.1];
-                            if v.len() != 0 {
-                                for ix in v {
-                                    m[r][r + l].push(*ix);
-                                }
-                            } else {
-                                for idx in 0..rules.len() {
-                                    if rules[idx].contains(&subrules[*pair.0][*pair.1]) {
-                                        bag[*pair.0][*pair.1].push(idx);
-                                        m[r][r + l].push(idx);
-                                    }
-                                }
-                            }
-                        }
-                        m[r][r + l].sort();
-                        m[r][r + l].dedup();
-                    }
-                }
-            }
-
-            if m[0][line.len() - 1].len() != 0 {
-                println!("ok");
-            } else {
-                println!("not ok");
-            }
-        }
-    }
+    let rules = parse_rules(lines.next().unwrap().split("\n").collect());
+    let answer = lines
+        .next()
+        .unwrap()
+        .split("\n")
+        .filter(|line| parse(&rules, line))
+        .count();
+    println!("{}", answer);
 
     Ok(())
 }
 
-fn make_subrule(tuple: (&usize, &usize)) -> String {
-    let mut s = tuple.0.to_string();
-    s.push_str(&String::from(" "));
-    s.push_str(&tuple.1.to_string());
-    s
+fn parse_rules(raw: Vec<&str>) -> Vec<Rule> {
+    let mut rules: Vec<Rule> = vec![Rule::NonTerminating(Vec::new()); raw.len()];
+    for rule_proto in raw {
+        let mut chunks = rule_proto.split(":");
+        let idx = chunks.next().unwrap().parse::<usize>().unwrap();
+        if rule_proto.contains("\"") {
+            let mut rule = chunks.next().unwrap().to_string();
+            rule.retain(|c| c != '\"');
+            rules[idx] = Rule::Terminating(rule.chars().collect::<Vec<char>>()[1]);
+        } else {
+            let subrules: Vec<Vec<usize>> = chunks
+                .next()
+                .unwrap()
+                .split("|")
+                .map(|subrule_raw| {
+                    subrule_raw
+                        .trim()
+                        .split(" ")
+                        .map(|i| i.parse::<usize>().unwrap())
+                        .collect()
+                })
+                .collect();
+            rules[idx] = Rule::NonTerminating(subrules);
+        }
+    }
+
+    rules
 }
